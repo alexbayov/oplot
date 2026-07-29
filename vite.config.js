fba4e04db4acf697df30dc2f46793b68f586bb3a
System Admin <admin@oplot.local>
2026-07-29 11:18:54 +0000

    fix: increase chunk size warning limit

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    chunkSizeWarningLimit: 5000, // 5MB warning limit
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          babylon: ['@babylonjs/core'],
-        }
-      }
-    }
+    chunkSizeWarningLimit: 5000,
   }
 })
